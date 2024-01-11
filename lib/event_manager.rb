758c137b14ec5a4b5b8ffaa7dd55702439b62f75
pwdcat <113306139+pwdcat@users.noreply.github.com>
2024-01-11 15:26:36 -0500

    Iteration 4: Form letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'erb'
 require 'google/apis/civicinfo_v2'
 
 civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
@@ -15,32 +16,44 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names = legislator_names.join(', ')
+    ).officials
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 contents = CSV.open(
   'event_attendees.csv',
   headers: true,
   header_converters: :symbol
 )
 
+# template_letter = File.read('form_letter.html')
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
 end
 
 # def clean_zipcode(zipcode)
